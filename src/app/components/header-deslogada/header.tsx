724fc3b6ff3da2af1adfd7b24556edcd76aba949
Amanda Hellen <amanda.hellen@g.globo>
2024-11-12 16:57:46 -0300

    feat: footer changes

--- src/app/components/header-deslogada/header.tsx
+++ src/app/components/header-deslogada/header.tsx
@@ -12,7 +12,7 @@ const HeaderDeslogada = () => {
     <>
       <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-20 px-6 bg-[#FFFFFF] text-[#5B5858] text-xl font-sans z-50">
         <div className="flex items-center">
-          <img src="/img/logo.png" alt="Logo" className="h-12" />
+          <img src="/img/logo-white.png" alt="Logo" className="h-12" />
         </div>
         <div className="md:hidden">
           <button
--- src/app/components/section-footer/footer.tsx
+++ src/app/components/section-footer/footer.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 const SectionFooter = () => {
   return (
-    <footer className="bg-[#FFFFFF] px-8 sm:px-16 lg:px-40 py-16 font-archivo-bold">
+    <footer className="bg-gradient-to-r from-[#a735b5] to-[#b33375] px-8 sm:px-16 lg:px-40 py-16 font-archivo-bold">
       <div className="flex flex-col lg:flex-row lg:justify-between gap-8 lg:gap-0">
         <div className="flex flex-col w-full max-w-[245px] lg:max-w-[245px] leading-5">
           <Image
-            src="/img/logo.png"
+            src="/img/logo-degrade.png"
             alt="Logo"
             width={200}
             height={25}
-            className="mb-4"
+            className="mb-4 object-contain"
           />
-          <p className="text-[#5B5858] text-sm sm:text-base">
+          <p className="text-[#FFFFFF] text-sm sm:text-base">
             O ColabTask é uma plataforma prática para equipes gerenciarem
             tarefas juntas, com atualização em tempo real e interface moderna
             para mais produtividade e organização.
@@ -23,7 +23,7 @@ const SectionFooter = () => {
             <li>
               <a
                 href="#"
-                className="text-[#5B5858] hover:text-[#913276] text-sm sm:text-base"
+                className="text-[#FFFFFF] hover:text-[#913276] text-sm sm:text-base"
               >
                 Início
               </a>
@@ -31,7 +31,7 @@ const SectionFooter = () => {
             <li>
               <a
                 href="#sobre"
-                className="text-[#5B5858] hover:text-[#913276] text-sm sm:text-base"
+                className="text-[#FFFFFF] hover:text-[#913276] text-sm sm:text-base"
               >
                 Sobre
               </a>
@@ -39,7 +39,7 @@ const SectionFooter = () => {
             <li>
               <a
                 href="#planos"
-                className="text-[#5B5858] hover:text-[#913276] text-sm sm:text-base"
+                className="text-[#FFFFFF] hover:text-[#913276] text-sm sm:text-base"
               >
                 Planos
               </a>
@@ -47,7 +47,7 @@ const SectionFooter = () => {
             <li>
               <a
                 href="#avaliaçoes"
-                className="text-[#5B5858] hover:text-[#913276] text-sm sm:text-base"
+                className="text-[#FFFFFF] hover:text-[#913276] text-sm sm:text-base"
               >
                 Avaliações
               </a>
@@ -55,7 +55,7 @@ const SectionFooter = () => {
             <li>
               <a
                 href="#faq"
-                className="text-[#5B5858] hover:text-[#913276] text-sm sm:text-base"
+                className="text-[#FFFFFF] hover:text-[#913276] text-sm sm:text-base"
               >
                 FAQ
               </a>
@@ -64,7 +64,7 @@ const SectionFooter = () => {
         </div>
         <div className="flex flex-col justify-start gap-4 sm:gap-6 lg:items-start">
           <div className="flex items-center gap-2 sm:gap-4 justify-start">
-            <p className="text-[#5B5858] text-xl">Siga: </p>
+            <p className="text-[#FFFFFF] text-xl">Siga: </p>
             <a href="" target="_blank" aria-label="Instagram">
               <Image
                 src="/img/img-instagram.svg"
@@ -86,11 +86,11 @@ const SectionFooter = () => {
           </div>
         </div>
       </div>
-      <div className="text-[#5B5858] mt-20 flex flex-col sm:flex-row lg:flex-row gap-4 sm:gap-6 lg:gap-8 items-start lg:justify-between justify-start">
+      <div className="text-[#FFFFFF] mt-20 flex flex-col sm:flex-row lg:flex-row gap-4 sm:gap-6 lg:gap-8 items-start lg:justify-between justify-start">
         <p className="text-left text-sm sm:text-base">
           Política de privacidade
         </p>
-        <p className="text-left text-sm sm:text-base">
+        <p className="text-left text-[#FFFFFF] text-sm sm:text-base">
           © Copyright 2000-2024 ColabTask
         </p>
       </div>
